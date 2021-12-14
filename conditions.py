d2539982268b381d10aaf3fe7a9009e216f6bd8b
dia36u <dia.babacar1@gmail.com>
2021-12-14 15:42:18 +0100

    ajout des exercices conditions et fonctions1

---
+++ conditions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Created on Tue Dec 14 13:34:07 2021
+
+# @author: diaba
+
+#%%
+#### True and False
+
+print(1 == 1)
+print(3 == 3.0)
+print(1+3 <= 5)
+print(5 >= 5.0)
+print(1 >= 0)
+
+print(52-2>=51)
+print('a' == 'A')
+print('007' == 'OO7')
+print(0>=0.5)
+print(12<11)
+#%%
+##Tree is a Crowd
+names=['jacques','henry','david','paul']
+if names >3:
+    print('There is a lot of people here!')
+ 
---
+++ fonction1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# """
+# Created on Tue Dec 14 15:02:27 2021
+
+# @author: diaba
+# """
+
+names=['babacar','thomas','agathe']
+lastnames=['dia','edison','oderi']
+
+#%%
+## Greater
+
+def congrats(name):
+    print('congratulations %s! \n Your are awesome %s ! \n Keep it up %s!\n'%(name.title(),name.title(),name.title()))
+
+for name in names:
+    congrats(name)
+#%%
+## Full Names
+
+def full_name(firstname,lastname):
+    print('Hello, %s %s.'%(firstname.upper(),lastname.upper()))
+
+full_name(names[0],lastnames[0])
+full_name(names[1],lastnames[1])
+full_name(names[2],lastnames[2])
+#%%
+## Addition Calculator
+
+def addition(num1,num2):
+    print('%i + %i = %i'%(num1,num2,num1+num2))
+
+addition(2,3)
+addition(10,40)
+addition(20,1)
+
+#%%
+## Return Calculator
+
+def returnCalculator(num1,num2):
+    return num1+num2
+
+num1=[2,10,20]
+num2=[3,40,1]
+
+print('%i + %i = %i'%(num1[0],num2[0],returnCalculator(num1[0],num2[0])))
+print('%i + %i = %i'%(num1[1],num2[1],returnCalculator(num1[1],num2[1])))
+print('%i + %i = %i'%(num1[2],num2[2],returnCalculator(num1[2],num2[2])))
